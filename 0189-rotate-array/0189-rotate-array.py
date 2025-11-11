e17495effa2772354da639378de0a8346064331a
Bhaskara Vanacharla <141829797+vbvss199@users.noreply.github.com>
2025-11-10 23:08:36 -0500

    Time: 8 ms (29.1%), Space: 25.6 MB (47.61%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.py
@@ -0,0 +1,12 @@
+class Solution:
+    def rev(self,nums,left,right):
+            while left <right:
+                nums[left], nums[right]=nums[right], nums[left]
+                left=left+1
+                right=right-1
+    def rotate(self, nums: List[int], k: int) -> None:
+        n=len(nums)
+        k=k%n
+        self.rev(nums,0,n-1)
+        self.rev(nums,0,k-1)
+        self.rev(nums,k,n-1)
