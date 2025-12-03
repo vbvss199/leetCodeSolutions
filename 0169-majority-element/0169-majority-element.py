b521e85e0f6244ebeec303e1f2e4ce3bbdaf352d
Bhaskara Vanacharla <141829797+vbvss199@users.noreply.github.com>
2025-12-03 18:22:32 -0500

    Time: 3 ms (83.44%), Space: 19.4 MB (51.24%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,8 +1,22 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        freqMap={}
-        n=len(nums)
+        # freqMap={}
+        # n=len(nums)
+        # for i in range(len(nums)):
+        #     freqMap[nums[i]]=freqMap.get(nums[i],0)+1
+        # max_key = max(freqMap, key=freqMap.get)
+        # return max_key
+
+        # let's try the moore's voting algorithm
+        element =nums[0]
+        count=0
         for i in range(len(nums)):
-            freqMap[nums[i]]=freqMap.get(nums[i],0)+1
-        max_key = max(freqMap, key=freqMap.get)
-        return max_key
+            if(nums[i]==element):
+                count=count+1
+            else:
+                count=count-1
+                if(count==0):
+                    element=nums[i+1]
+                else:
+                    continue
+        return element 
