05f6f64252d84c3ed228c7950dfafff5c81a1ba4
Bhaskara Vanacharla <141829797+vbvss199@users.noreply.github.com>
2025-12-04 19:26:29 -0500

    Time: 39 ms (80.04%), Space: 42.1 MB (75.43%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
@@ -1,13 +1,13 @@
 class Solution:
     def rearrangeArray(self, nums: List[int]) -> List[int]:
-        newarr=[]
+        newarr=[0]*len(nums)
         odd=1
         even=0
         for i in range(len(nums)):
             if(nums[i]>0):
-                newarr.insert(even,nums[i])
+                newarr[even]=nums[i]
                 even=even+2
             else:
-                newarr.insert(odd,nums[i])
+                newarr[odd]=nums[i]
                 odd=odd+2
         return newarr
