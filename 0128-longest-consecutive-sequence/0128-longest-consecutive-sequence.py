e06afc3095beafd98bc9718c9bdabb93d516859d
Bhaskara Vanacharla <141829797+vbvss199@users.noreply.github.com>
2025-12-09 12:18:42 -0500

    Time: 101 ms (11.58%), Space: 37.8 MB (12.25%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -5,6 +5,7 @@ class Solution:
         maxCount=0
         for i in range(len(nums)):
              freqMap[nums[i]] = freqMap.get(nums[i], 0) + 1
+        # this can be reduced in order to remove the loop time complexity above and can be converted to set which reduces the TC
         for x in freqMap:
             if x - 1 not in freqMap:
                 currentCount=1
