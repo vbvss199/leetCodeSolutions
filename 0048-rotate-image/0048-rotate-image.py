ea7e9c38b8e1f45e709dc02d93c0c2c41f660942
Bhaskara Vanacharla <141829797+vbvss199@users.noreply.github.com>
2025-12-10 11:08:11 -0500

    Time: 0 ms (100%), Space: 17.9 MB (22.63%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -3,10 +3,20 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        copy = [row[:] for row in matrix]
-        # as rows and columns are same so n is same
-        n=len(matrix[0])
+        # # create a deep copy instead of shallow ones 
+        # copy = [row[:] for row in matrix]
+        # # as rows and columns are same so n is same
+        # n=len(matrix[0])
+        # for i in range(n):
+        #     for j in range(n):
+        #         matrix[j][n-1-i]=copy[i][j]
+        # return matrix
+
+        # lets solve this in O(1) space complexity 
+        n=len(matrix)
         for i in range(n):
-            for j in range(n):
-                matrix[j][n-1-i]=copy[i][j]
+            for j in range(i+1,n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        for row in matrix:
+            row.reverse()
         return matrix
