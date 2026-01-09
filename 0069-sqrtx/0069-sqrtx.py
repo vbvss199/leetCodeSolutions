8e63a5935f5e0a5615393aed1f307a8e0dbc3abd
Bhaskara Vanacharla <141829797+vbvss199@users.noreply.github.com>
2026-01-08 19:40:31 -0500

    Time: 0 ms (100%), Space: 19.5 MB (9.29%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,12 +1,27 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        ans=1
-        if(x==0):
-            return 0
-        else:
-            for i in range(1,x):
-                if i*i<=x:
-                    ans=i
-                else:
-                    break
+        # ans=1
+        # if(x==0):
+        #     return 0
+        # else:
+        #     for i in range(1,x):
+        #         if i*i<=x:
+        #             ans=i
+        #         else:
+        #             break
+        # return ans
+        
+        # lets try the binary search approach
+        ans = 0
+        low=1
+        high=x
+        while(low<=high):
+            mid=(low+high)//2
+            if (mid*mid == x):
+                return mid
+            elif(mid*mid < x):
+                ans = mid
+                low=mid+1
+            else:
+                high=mid-1
         return ans
