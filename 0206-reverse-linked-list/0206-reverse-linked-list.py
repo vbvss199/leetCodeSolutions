33c64c73a3d75f8b85366f06b906ee7917b34ad5
Bhaskara Vanacharla <141829797+vbvss199@users.noreply.github.com>
2026-01-26 21:53:34 -0500

    Time: 0 ms (100%), Space: 20.7 MB (11.57%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -5,11 +5,23 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        prev=None
-        curr=head
-        while curr:
-            nxt=curr.next #save next
-            curr.next=prev  #reverse the pointers 
-            prev=curr
-            curr=nxt
-        return prev
+        # prev=None
+        # curr=head
+        # while curr:
+        #     nxt=curr.next #save next
+        #     curr.next=prev  #reverse the pointers 
+        #     prev=curr #move previous pointer
+        #     curr=nxt #move curr pointer 
+        # return prev
+
+        # naive approach using stack lifo principle
+        stack=[]
+        temp=head
+        while(temp!=None):
+            stack.append(temp.val)
+            temp=temp.next
+        temp=head
+        while(temp!=None):
+            temp.val=stack.pop()
+            temp=temp.next
+        return head
