c5d591ac96d466e74594171ca106492dcab9a051
Bhaskara Vanacharla <141829797+vbvss199@users.noreply.github.com>
2026-01-31 22:18:38 -0500

    Time: 0 ms (100%), Space: 19.3 MB (43.65%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -5,41 +5,18 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        # naive solution 
-        # temp1=list1
-        # temp2=list2
-        # li=[]
-        # while(temp1!=None):
-        #     li.append(temp1.val)
-        #     temp1=temp1.next
-        # while(temp2!=None):
-        #     li.append(temp2.val)
-        #     temp2=temp2.next
-        # li.sort()
-        # dummy_node=ListNode(0)
-        # current=dummy_node
-        # for i in range(len(li)):
-        #     current.next=ListNode(li[i])
-        #     current=current.next
-        # return dummy_node.next
-
-        # optimal solution   
-        temp1=list1
-        temp2=list2
-        dummy=ListNode(0)
-        temp=dummy
-        while(temp1!=None and temp2!=None):
-            if(temp1.val<temp2.val):
-                temp.next=temp1
-                temp=temp1
-                temp1=temp1.next
+        dummy = ListNode(-1)
+        temp = dummy
+        while list1 and list2:
+            if list1.val<=list2.val:
+                temp.next=list1
+                list1=list1.next
             else:
-                temp.next=temp2
-                temp=temp2
-                temp2=temp2.next
-        if(temp1):
-            temp.next=temp1
-        else:
-            temp.next=temp2
+                temp.next=list2
+                list2=list2.next
+            temp=temp.next
+        if(list1):
+            temp.next=list1
+        if(list2):
+            temp.next=list2
         return dummy.next
-        
