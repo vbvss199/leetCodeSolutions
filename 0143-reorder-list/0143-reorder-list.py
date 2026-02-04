c10205ecbb569ed3c59272bb9b0c5bbeca46ee5e
Bhaskara Vanacharla <141829797+vbvss199@users.noreply.github.com>
2026-02-04 14:59:17 -0500

    Time: 0 ms (100%), Space: 28.1 MB (13.05%) - LeetHub

---
+++ 0143-reorder-list/0143-reorder-list.py
@@ -0,0 +1,55 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self,slow:Optional[ListNode]) -> None:
+        curr=slow
+        prev=None
+        while(curr!=None):
+            # save the next pointer
+            nxt=curr.next
+
+            # reverse the pointer directions 
+            curr.next=prev
+
+            # interchange the current and preious
+            prev=curr
+
+            # reassign the nxt pointer to current
+            curr=nxt
+        return prev 
+
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        # swap logic between the last element and the first element and move the first pointer!!!
+        if not head or not head.next:
+            return
+        slow=head
+        fast=head
+        while(fast!=None and fast.next!=None):
+            slow=slow.next
+            fast=fast.next.next
+        # reverse the list from slow and merge alternatively
+        right=self.reverseList(slow.next)
+        # cut the link
+        slow.next=None
+        left=head
+
+        # now merge both the right and left alternatively 
+        while(right!=None):
+            rightnext=right.next
+            leftnext=left.next
+
+            left.next=right
+
+            # after that link the lists so 
+            right.next=leftnext
+
+            left=leftnext
+            right=rightnext
+        return head
+
