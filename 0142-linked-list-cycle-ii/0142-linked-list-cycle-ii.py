925ee67368968e96c724e82d5e1b3b2a25a1a009
Bhaskara Vanacharla <141829797+vbvss199@users.noreply.github.com>
2026-02-04 13:11:12 -0500

    Time: 37 ms (96.49%), Space: 22.4 MB (37.47%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
@@ -6,14 +6,31 @@
 
 class Solution:    
     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # naive approach
-        temp=head
-        freqMap={}
-        while(temp!=None):
-            if temp in freqMap:
-                return temp
-            else:
-                freqMap[temp]=1
-            temp=temp.next
+        # # naive approach
+        # temp=head
+        # freqMap={}
+        # while(temp!=None):
+        #     if temp in freqMap:
+        #         return temp
+        #     else:
+        #         freqMap[temp]=1
+        #     temp=temp.next
+        # # finally if it is a linear linkedlist we need to return NUll
+        # return None
         # ok if cycle exists then we need to return the point where the cycle is starting not the meeting point 
         # it is not necessarily they should meet at the same point always 
+
+        # Optimal approach !?????
+        slow=head
+        fast=head
+        while fast and fast.next:
+            slow=slow.next
+            fast=fast.next.next
+            if(slow==fast):
+                break
+        slow=head
+        while(fast and fast.next):
+            if(slow==fast):
+                return fast
+            slow=slow.next
+            fast=fast.next
