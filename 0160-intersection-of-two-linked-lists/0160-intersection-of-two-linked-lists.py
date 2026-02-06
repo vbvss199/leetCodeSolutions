ca15e7ec2888a8019b0dc65e152aaf76a96fb5f4
Bhaskara Vanacharla <141829797+vbvss199@users.noreply.github.com>
2026-02-05 20:26:11 -0500

    Time: 106 ms (90.18%), Space: 38.1 MB (24.22%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
@@ -6,17 +6,53 @@
 
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
-        freqMap={}
+        # freqMap={}
+        # temp1=headA
+        # while(temp1!=None):
+        #     if temp1 in freqMap:
+        #         return temp1
+        #     freqMap[temp1]=1
+        #     temp1=temp1.next
+        # temp2=headB
+        # while(temp2!=None):
+        #     if temp2 in freqMap:
+        #         return temp2
+        #     temp2=temp2.next
+        # return None
+
+        # compute the length 1
         temp1=headA
+        lengthA=0
         while(temp1!=None):
-            if temp1 in freqMap:
-                return temp1
-            freqMap[temp1]=1
+            lengthA=lengthA+1
             temp1=temp1.next
+        # computee the length 2
         temp2=headB
+        lengthB=0
         while(temp2!=None):
-            if temp2 in freqMap:
-                return temp2
+            lengthB=lengthB+1
             temp2=temp2.next
-        return None
+        # move the pointer by length2-length 1 times 
+        if(lengthA>lengthB):
+            # move the linkedlistA by d times
+            temp1=headA
+            temp2=headB
+            k=lengthA-lengthB
+            while(k!=0):
+                temp1=temp1.next
+                k=k-1
+        # move the linkedlistB by d times
+        else:
+            temp2=headB
+            temp1=headA
+            k= lengthB-lengthA
+            while(k!=0):
+                temp2=temp2.next
+                k=k-1
+        # then move the temp1 and temp2 by one unit each and start comparing if they r same return the pointer 
+        while(temp1!=None and temp2!=None):
+            if(temp1==temp2):
+                return temp1
+            temp1=temp1.next
+            temp2=temp2.next
         
