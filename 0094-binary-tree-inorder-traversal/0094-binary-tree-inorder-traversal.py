3832dc5980bc62167fd66dc52c6cb0c26ec16da1
Bhaskara Vanacharla <141829797+vbvss199@users.noreply.github.com>
2026-02-12 12:38:35 -0500

    Time: 0 ms (100%), Space: 19.4 MB (18.35%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
@@ -7,12 +7,62 @@
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         result=[]
-        def inorder(node):
-            if node is None:
-                return []
-            else:
-                inorder(node.left)
-                result.append(node.val)
-                inorder(node.right)
-        inorder(root)
-        return result
+        # def inorder(node):
+        #     if node is None:
+        #         return
+        #     else:
+        #         inorder(node.left)
+        #         result.append(node.val)
+        #         inorder(node.right)
+        # inorder(root)
+        # return result
+        if root is None:
+            return []
+        else:
+            stack=[]
+            curr=root
+            while(curr or stack):
+                while curr:
+                    stack.append(curr)
+                    curr=curr.left
+                # process the current node
+                curr=stack.pop()
+                result.append(curr.val)
+                curr=curr.right
+            return result
+
+
+
+
+
+
+
+
+
+
+
+#       1
+#      / \
+#     2   3
+#    /
+#   4
+
+# Inorder = Left → Root → Right
+# Let’s trace:
+# Start at 1
+# Go left → 2
+# Go left → 4
+# Go left → None (stop)
+# Append 4
+# Go right of 4 → None
+# Back to 2
+# Append 2
+# Go right of 2 → None
+# Back to 1
+# Append 1
+# Go right → 3
+# Append 3
+
+# Go left recursively
+# Append current node value
+# Go right recursively
