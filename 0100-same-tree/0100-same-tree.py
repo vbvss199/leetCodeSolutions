55d513fc6b9b1c48402b02f2286b266f82d3c856
Bhaskara Vanacharla <141829797+vbvss199@users.noreply.github.com>
2026-02-15 23:46:13 -0500

    Time: 0 ms (100%), Space: 19.4 MB (23.68%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -7,10 +7,34 @@
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
         # how to approach this ???t
-        if p is None and q is None:
+        # if p is None and q is None:
+        #     return True
+        # if p is None or q is None:
+        #     return False
+        # if(p.val!=q.val):
+        #     return False
+        # return self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)
+        # # mottam recursion tappa em ledhu !!!
+
+        # this can be done using the pre order and in order traversal as well do the pre order and inorder traversak and compare both the arrays 
+        # lets do this approach Approach2
+        l1=self.preOrder(p)
+        l2=self.preOrder(q)
+        if l1==l2:
             return True
-        if p is None or q is None:
-            return False
-        if(p.val!=q.val):
-            return False
-        return self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)
+        else:
+            return False 
+    # to calculate the in order of tree 
+    def preOrder(self,root:Optional[TreeNode])->list:
+        l=[]
+        if root is None :
+            l.append(None)
+            return l
+        else:
+            l.append(root.val)
+            l=l+self.preOrder(root.left)
+            l=l+self.preOrder(root.right)
+        return l
+
+
+# for any pre or in or post to work we need to cosidet the None as well so if it is none 
