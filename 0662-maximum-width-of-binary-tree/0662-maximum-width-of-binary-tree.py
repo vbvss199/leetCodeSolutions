627e03ba14cc5a774b5fec7bfa73f330d2a7e7ba
Bhaskara Vanacharla <141829797+vbvss199@users.noreply.github.com>
2026-02-18 23:57:21 -0500

    Time: 4 ms (37.53%), Space: 20.3 MB (18.68%) - LeetHub

---
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from collections import deque
+class Solution:
+    def levelOrderTraversal(self,root:Optional[TreeNode])->int:
+        if root is None:
+            return []
+        queue=deque()
+        queue.append((root,0))
+        while(queue):
+            first_index=queue[0][1]
+            level=[]
+            size=len(queue)
+            for _ in range(size):
+                node,index=queue.popleft()
+                last_index = index
+                if node.left:
+                    queue.append((node.left,2*index+1))
+                if node.right:
+                    queue.append((node.right,2*index+2))
+            self.maxWidth=max(self.maxWidth,last_index-first_index+1)
+        return self.maxWidth
+
+        
+    def widthOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        # add all the levels into the list and after adding return the one which has the maximum 
+        # before that if we encounter the null in the level order traversal add that as well
+        self.maxWidth=float("-inf")
+        return self.levelOrderTraversal(root)
+        # write a logic to pull out the maximum 
