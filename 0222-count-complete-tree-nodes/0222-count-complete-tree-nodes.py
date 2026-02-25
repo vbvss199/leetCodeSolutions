54055ccd69ef1d993f66d2264e5f3f06229a010e
Bhaskara Vanacharla <141829797+vbvss199@users.noreply.github.com>
2026-02-24 21:34:39 -0500

    Time: 0 ms (100%), Space: 24.1 MB (18.82%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
@@ -5,16 +5,42 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    def findLeftHeight(self,root:Optional[TreeNode])->int:
+        height=0
+        while(root):
+            height=height+1
+            root=root.left
+        return height
+    
+    def findRightHeight(self,root:Optional[TreeNode])->int:
+        height=0
+        while(root):
+            height=height+1
+            root=root.right
+        return height
+
     def countNodes(self, root: Optional[TreeNode]) -> int:
         # traverse in any order then increment the node values lets go with the pre order
-        self.nodeCount=0
-        def dfs(node):
-            if node is None:
-                return
-            else:
-                self.nodeCount=self.nodeCount+1
-                dfs(node.left)
-                dfs(node.right)
-        dfs(root)
-        return self.nodeCount
+        # self.nodeCount=0
+        # def dfs(node):
+        #     if node is None:
+        #         return
+        #     else:
+        #         self.nodeCount=self.nodeCount+1
+        #         dfs(node.left)
+        #         dfs(node.right)
+        # dfs(root)
+        # return self.nodeCount
         
+
+        # lets try the optimised one 
+        if root is None:
+            return 0
+        lh=self.findLeftHeight(root)
+        rh=self.findRightHeight(root)
+        # case of complete binary tree
+        if(lh==rh):
+            return 2**lh-1
+
+        # if it is not a complete binary tree
+        return 1+self.countNodes(root.left)+self.countNodes(root.right)
