349058965571e2f3c2406cc4d4d369cec982534e
Bhaskara Vanacharla <141829797+vbvss199@users.noreply.github.com>
2026-02-27 00:35:12 -0500

    Time: 0 ms (100%), Space: 19.7 MB (36.92%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.py
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.py
@@ -5,19 +5,35 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def __init__(self):
-        self.prev = None
+    # def __init__(self):
+    #     self.prev = None
     def flatten(self, root: Optional[TreeNode]) -> None:
-        """
-        Do not return anything, modify root in-place instead.
-        """
-        # left pointer is always null and right should point to the next!
+        # """
+        # Do not return anything, modify root in-place instead.
+        # """
+        # # left pointer is always null and right should point to the next!
+        # if root is None:
+        #     return
+        # self.flatten(root.right)
+        # self.flatten(root.left)
+        # root.right=self.prev
+        # root.left=None
+        # self.prev=root
+        
+        
+        # lets go with the second approach stack
         if root is None:
-            return
-        self.flatten(root.right)
-        self.flatten(root.left)
-        root.right=self.prev
-        root.left=None
-        self.prev=root
-        
-        
+            return []
+        else:
+            stack=[]
+            stack.append(root)
+            while(stack):
+                current=stack.pop()
+                if(current.right):
+                    stack.append(current.right)
+                if(current.left):
+                    stack.append(current.left)
+                if(stack):
+                    current.right=stack[-1]
+                current.left=None
+    
