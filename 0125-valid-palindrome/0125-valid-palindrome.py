7b6a7bcad752dfde6f735e4616c20bf4422e3770
Bhaskara Vanacharla <141829797+vbvss199@users.noreply.github.com>
2026-03-12 01:42:30 -0400

    Time: 12 ms (15.58%), Space: 31.3 MB (5.6%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,15 +1,14 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         # reverse the string and it reads the same then return true  
-        def isPalindromeRecursive(left,right):
-            if(left>=right):
+        def isPalindromeRecursive(i):
+            if(i>=n/2):
                 return True
             # write the base case 
-            if(s[left]!=s[right]):
+            if(s[i]!=s[n-i-1]):
                 return False
-            return isPalindromeRecursive(left+1,right-1)
+            return isPalindromeRecursive(i+1)
         # make sure to split the array using split
         s=[c.lower() for c in s if c.isalnum()]
-        left=0
-        right=len(s)-1
-        return isPalindromeRecursive(left,right)
+        n=len(s)
+        return isPalindromeRecursive(0)
