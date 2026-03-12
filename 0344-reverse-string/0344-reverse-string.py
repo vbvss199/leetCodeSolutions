e3d5d8de0150aebf5132ba3b08274f750e2562a0
Bhaskara Vanacharla <141829797+vbvss199@users.noreply.github.com>
2026-03-12 00:39:49 -0400

    Time: 3 ms (47.79%), Space: 29.6 MB (20.09%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -3,10 +3,19 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        i=0
-        j=len(s)-1
-        while(i<j):
-            s[i],s[j]=s[j],s[i]
-            i=i+1
-            j=j-1
-        return s
+        # i=0
+        # j=len(s)-1
+        # while(i<j):
+        #     s[i],s[j]=s[j],s[i]
+        #     i=i+1
+        #     j=j-1
+        # return s
+
+        # lets do the same operation with recursion 
+        #function(l,r) swap(l,r) function(l+1,r-1) and the base condition is if(l>=r): return 
+        def recursiveReverseString(left,right):
+            if left>=right:
+                return
+            s[left],s[right]=s[right],s[left]
+            recursiveReverseString(left+1,right-1)
+        recursiveReverseString(0,len(s)-1)
