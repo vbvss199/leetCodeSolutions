da8dd2751a09a3afb8ab9641ac2f03fcdcaba4ba
Bhaskara Vanacharla <141829797+vbvss199@users.noreply.github.com>
2026-03-13 00:48:19 -0400

    Time: 253 ms (83.27%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -4,10 +4,12 @@
 -- LIMIT 1 OFFSET 1 ) as SecondHighestSalary
 
 -- USING IF NULL
-SELECT IFNULL(
-    (
-        SELECT DISTINCT salary 
-        FROM Employee
-        ORDER BY salary DESC
-        LIMIT 1 OFFSET 1
-    ),NULL) AS SecondHighestSalary;
+-- SELECT IFNULL(
+--     (
+--         SELECT DISTINCT salary 
+--         FROM Employee
+--         ORDER BY salary DESC
+--         LIMIT 1 OFFSET 1
+--     ),NULL) AS SecondHighestSalary;
+-- the inner query returns the salary and its ranks 
+SELECT IFNULL((SELECT salary FROM (SELECT salary,DENSE_RANK() OVER(ORDER BY salary DESC) as rnk FROM Employee) t WHERE rnk=2 LIMIT 1),NULL) AS SecondHighestSalary
