7cf59c58ed62738ed8582c9ceaacd95d64ec2121
Bhaskara Vanacharla <141829797+vbvss199@users.noreply.github.com>
2026-06-11 22:08:42 -0400

    Time: 0 ms (100%), Space: 19.1 MB (90.21%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.py
@@ -0,0 +1,40 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        # the answer may not lie in the single house skipping it may lie in the two or more sometimes 
+        # this can be solved using the DP but think it , and more over we need to return the maximum here !!!!!
+        # one point is clear we need to go from starting to end 
+        #  in 2 1.4. 9 we pick 2 and 9 to get the max 
+        # sub array must be contiguous and 
+        #  sub sequence is something we can skip element but order must be preserved
+        # try out all the sub sequences and try out the one which has maximum sum 
+        # try recursion and see if there are overlapping sub problems and then we can go to the other 
+        # theres a technique called pick and non pick but picking and non picking make sure they are not adjacent ! and thats it then out of them pick the one which is best one !!!!!
+        # if we pick we take sum=a[index] and not pick will be 0+
+        # and one more thing if we check if i==0 then print it , we start from f(nums,n) and if we do i==n then it is f(nums,0)
+        # to store the occurances !!!
+        dp={}
+        return self.maxSubSequence(nums,0,dp)
+    # def maxSubSequence(self,nums: List[int],i:int) -> int:
+    #     if(i>=len(nums)):
+    #         #if we reach the end theers nothung 
+    #         return 0
+    #     #and track the sum using the pick and no pick variable 
+    #     pick=nums[i]+self.maxSubSequence(nums,i+2)
+    #     not_pick=0+self.maxSubSequence(nums,i+1)
+    #     sum=max(pick,not_pick)
+    #     return sum
+
+    
+    # so here comes the overlapping sub problem if we r calcualting anything repeatdly so use DP 
+    # but how come ? using the dict as in the evry moment we r storing the sum which is max instead store in the dp[i]
+    def maxSubSequence(self,nums: List[int],i:int,dp) -> int:
+        if(i>=len(nums)):
+            #if we reach the end theers nothung 
+            return 0
+        if i in dp:
+            return dp[i]
+        #and track the sum using the pick and no pick variable 
+        pick=nums[i]+self.maxSubSequence(nums,i+2,dp)
+        not_pick=0+self.maxSubSequence(nums,i+1,dp)
+        dp[i]=max(pick,not_pick)
+        return dp[i]
